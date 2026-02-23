a4c4226a6cee99f716d573cc87a56f97e77f168b
Nukaiah04 <yalagalanukaiah14@gmail.com>
2026-02-23 15:50:34 +0530

    add another extra

--- HealthcareApp/Program.cs
+++ HealthcareApp/Program.cs
@@ -1,8 +1,20 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "CI/CD Working Successfully in GCP 🚀");
+app.MapGet("/", () => "CI/CD Working Successfully in GCP 🚀 - FEATURE VERSION");
 
 app.MapGet("/health", () => "Healthy");
+
+// 🔥 New Feature Endpoint (Only in feature branch)
+app.MapGet("/feature-info", () =>
+{
+    return new
+    {
+        Project = "Simple Project",
+        Environment = "Feature Branch",
+        Status = "Working",
+        Time = DateTime.Now
+    };
+});
 
 app.Run();
