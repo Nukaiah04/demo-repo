4c7acb66ea4f859c7f4626f7ed304cf5c3d7c925
Nukaiah04 <yalagalanukaiah14@gmail.com>
2026-02-25 18:39:31 +0530

    Delete Program.cs

--- Program.cs
+++
@@ -1,8 +0,0 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
-
-app.MapGet("/", () => "CI/CD Working Successfully in GCP 🚀");
-
-app.MapGet("/health", () => "Healthy");
-
-app.Run();
